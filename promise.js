c255679642f0fdec9c7428aa8d794981e1fb9840
Shamanth0209 <shamanthkn2002@gmail.com>
2025-11-28 09:30:10 +0530

    version2

--- promise.js
+++ promise.js
@@ -1,45 +1,95 @@
-function fetchdata(callback) {
-  setTimeout(() => {
-    console.log("i am fetching data");
-    callback();
-  }, 4000);
-}
+// function fetchdata(callback) {
+//   setTimeout(() => {
+//     console.log("i am fetching data");
+//     callback();
+//   }, 4000);
+// }
 
-function Arrangedata(callback) {
-  setTimeout(() => {
-    console.log("i am arranging data");
-    callback();
-  }, 3000);
-}
+// function Arrangedata(callback) {
+//   setTimeout(() => {
+//     console.log("i am arranging data");
+//     callback();
+//   }, 3000);
+// }
 
-function displaydata(callback) {
-  setTimeout(() => {
-    console.log("i am displaying data");
-    if (callback) callback();
-  }, 2000);
-}
-// Call back hell problem
-fetchdata(() => {
-  Arrangedata(() => {
-    displaydata();
-  });
-});
+// function displaydata(callback) {
+//   setTimeout(() => {
+//     console.log("i am displaying data");
+//     if (callback) callback();
+//   }, 2000);
+// }
+// // Call back hell problem
+// fetchdata(() => {
+//   Arrangedata(() => {
+//     displaydata();
+//   });
+// });
 
 //to over come call back hell problem we use promise
 //Promise is an object which will either resolve or reject the value aka success or failure
-let lifeline = new Promise((success, failure) => {
-  let a = 3;
-  if (a > 5) {
-    success("success");
-  } else {
-    failure("failure");
-  }
-});
-console.log(lifeline);
-lifeline
+// let lifeline = new Promise((success, failure) => {
+//   let a = 3;
+//   if (a > 5) {
+//     success("success");
+//   } else {
+//     failure("failure");
+//   }
+// });
+// console.log(lifeline);
+// lifeline
+//   .then((message) => {
+//     console.log(message);
+//   })
+//   .catch((message) => {
+//     console.log(message);
+//   }); // since reject throws an error we use catch
+
+let isTrue = false;
+function fetchdata() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isTrue) {
+        resolve("i am Fetching the data");
+      } else {
+        reject("i am not Fetching the data");
+      }
+    }, 1000);
+  });
+}
+function arrangedata() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isTrue) {
+        resolve("i am arranging the data");
+      } else {
+        reject("i am not arranging the data");
+      }
+    }, 2000);
+  });
+}
+function displaydata() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isTrue) {
+        resolve("i am displaying data");
+      } else {
+        reject("i am not displaying data");
+      }
+    }, 3000);
+  });
+}
+fetchdata()
+  .then((message) => {
+    console.log(message);
+    return arrangedata();
+  })
+  .then((message) => {
+    console.log(message);
+    return displaydata();
+  })
   .then((message) => {
     console.log(message);
   })
-  .catch((message) => {
-    console.log(message);
-  }); // since reject throws an error we use catch
+  .catch((error) => {
+    console.log(error);
+  });
